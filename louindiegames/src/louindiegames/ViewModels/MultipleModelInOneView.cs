aa2897c6aab6f13f9a4305f39d73a6a105f6ec39
Alicia Sims <alicialsims@gmail.com>
2016-03-19 20:10:54 -0400

    Created a viewmodel but it DOESNT WORK

---
+++ louindiegames/src/louindiegames/ViewModels/MultipleModelInOneView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace louindiegames.ViewModels
+{
+    public class ViewModel
+    {
+        public IEnumerable<louindiegames.Models.Game> Games { get; set; }
+        public IEnumerable<louindiegames.Models.Creator>  Creators { get; set }
+
+    }
+}
